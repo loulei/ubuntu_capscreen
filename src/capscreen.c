75e8655e91c86bfc5e24990ba78b01eb29f2450a
loulei <loulei@jointlab.org>
2017-02-13 14:52:27 +0800

    capscreen

--- src/capscreen.c
+++ src/capscreen.c
@@ -13,6 +13,84 @@
 #include <linux/input.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <X11/Xlib.h>
+#include <jpeglib.h>
+
+int jpegWriteFileFromImage(char *filename, XImage *img){
+	FILE *fp;
+	struct jpeg_compress_struct cinfo;
+	struct jpeg_error_mgr jerr;
+
+	fp = fopen(filename, "wb");
+	if(!fp){
+		printf("open file %s fail!\n", filename);
+		return 0;
+	}
+
+	cinfo.err = jpeg_std_error(&jerr);
+	jpeg_create_compress(&cinfo);
+
+	jpeg_stdio_dest(&cinfo, fp);
+	cinfo.image_width = img->width;
+	cinfo.image_height = img->height;
+	cinfo.input_components = 3;
+	cinfo.in_color_space = JCS_RGB;
+
+	jpeg_set_defaults(&cinfo);
+	jpeg_start_compress(&cinfo, TRUE);
+
+	JSAMPROW row_pointer[1];
+	unsigned char *pBuf = (unsigned char *)malloc(3*img->width);
+	row_pointer[0] = pBuf;
+
+	int i = 0;
+	while(cinfo.next_scanline < cinfo.image_height){
+		int j = 0;
+		for(i=0; i<img->width; i++){
+			*(pBuf+j) = *(img->data + cinfo.next_scanline*img->bytes_per_line+i*4+2);
+			*(pBuf+j+1) = *(img->data + cinfo.next_scanline*img->bytes_per_line+i*4+1);
+			*(pBuf+j+2) = *(img->data + cinfo.next_scanline*img->bytes_per_line+i*4);
+			j+=3;
+		}
+		jpeg_write_scanlines(&cinfo, row_pointer, 1);
+	}
+	free(pBuf);
+	jpeg_finish_compress(&cinfo);
+	jpeg_destroy_compress(&cinfo);
+	return 1;
+}
+
+int capscreen(){
+	Window desktop;
+	Display *display;
+	XImage *img;
+
+	int screen_width;
+	int screen_height;
+
+	display = XOpenDisplay(NULL);
+	if(!display){
+		printf("cannot connect to local display\n");
+		return 0;
+	}
+
+	desktop = RootWindow(display, 0);
+	if(!desktop){
+		printf("cannot get root window\n");
+		return 0;
+	}
+
+	screen_width = DisplayWidth(display, 0);
+	screen_height = DisplayHeight(display, 0);
+
+	img = XGetImage(display, desktop, 0, 0, screen_width, screen_height, ~0, ZPixmap);
+
+	jpegWriteFileFromImage("screen.jpeg", img);
+
+	XFree(img);
+	XCloseDisplay(display);
+	return 1;
+}
 
 int main(void) {
 
@@ -33,7 +111,7 @@ int main(void) {
 		if(read(keys_fd, &t, sizeof(t)) == sizeof(t)){
 			if(t.type == EV_KEY){
 				if(t.value == 0 || t.value == 1){
-					printf("key %d %s\n", t.code, t.value ? "Pressed" : "Release");
+//					printf("key %d %s\n", t.code, t.value ? "Pressed" : "Release");
 					if(t.code == KEY_ESC){
 						break;
 					}
@@ -48,6 +126,11 @@ int main(void) {
 						mode_capscreen = 0;
 						if(shortcut_keys[0] == KEY_LEFTCTRL && shortcut_keys[1] == KEY_LEFTALT && shortcut_keys[2] == KEY_A){
 							printf("capscreen !!!\n");
+							if(capscreen()){
+								printf("capscreen success !\n");
+							}else{
+								printf("capscreen fail !\n");
+							}
 						}
 					}
 				}
